b7f3e4809bb364d70f6aaa2aedc95c3fb1d9454b
SEIN <macsein@seins-mbp.lan>
2019-11-08 21:38:56 +0630

    first commit

--- testapp/main.cpp
+++ testapp/main.cpp
@@ -1,15 +1,12 @@
-//
 //  main.cpp
 //  testapp
-//
 //  Created by SEIN on 11/8/19.
 //  Copyright © 2019 Gaban. All rights reserved.
-//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main(int argc, const char * argv[])
+{
     std::cout << "Hello, World!\n";
     return 0;
 }
